92d909b81226ecfe8b180ee71f96857f40a03460
anushkaekanayake <anushkahakmana@gmail.com>
2015-03-04 18:04:29 +0530

    Adding the console application project

---
+++ NightWing/NightWing/NightWing.cpp
@@ -0,0 +1,13 @@
+// NightWing.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Welcome"<<endl;
+	return 0;
+}
+
